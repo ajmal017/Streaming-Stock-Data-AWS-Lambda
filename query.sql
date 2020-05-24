887efbec0093a0999a1c48ace16fadfdf3f000ef
qchau96 <48965120+qchau96@users.noreply.github.com>
2020-05-24 18:40:36 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,10 @@
-SELECT name, SUBSTRING(ts, 12, 2) AS hour, MAX(high) AS mhigh
-FROM "20" 
-GROUP BY 1,2
-ORDER BY 1,2
+SELECT name, ts, hour, high
+FROM
+(SELECT name, ts, SUBSTRING(ts, 12, 2) AS hour, high, RANK () OVER ( 
+			PARTITION BY name, SUBSTRING(ts, 12, 2)
+			ORDER BY high DESC, ts DESC
+		) price_rank 
+FROM "20") t1
+WHERE t1.price_rank = 1
+ORDER BY 1, 3
+
